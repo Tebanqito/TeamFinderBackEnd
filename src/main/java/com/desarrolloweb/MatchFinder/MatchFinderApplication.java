73f9b0c4052d01bfcd1600865a84c4d40c2b8437
yemesesteban@gmail.com <yemesesteban@gmail.com>
2026-06-23 20:33:25 -0300

    correccion del nombre del Artifact

---
+++ src/main/java/com/desarrolloweb/MatchFinder/MatchFinderApplication.java
@@ -0,0 +1,13 @@
+package com.desarrolloweb.MatchFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desarrolloweb/MatchFinder/MatchFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desarrolloweb.MatchFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
