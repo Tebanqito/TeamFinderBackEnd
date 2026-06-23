a15f830d063bacc18e4c6e6c3f53512f4a51e30e
yemesesteban@gmail.com <yemesesteban@gmail.com>
2026-06-23 19:44:39 -0300

    first commit

---
+++ src/test/java/com/desarrolloweb/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desarrolloweb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
